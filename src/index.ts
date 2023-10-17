4b48bbd5b3562e59ecf6e202b9d3f7d7b4bb6b91
sylvain kadjo <sylvainka12@gmail.com>
2023-10-17 21:30:49 +0400

    First Project init

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", () => "Personal website version 3").listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
